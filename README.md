8a074c9487ffae6f7c4de3e6ca0ba5e9fd430bd9
Cliff Parnitzky <github@cliff-parnitzky.de>
2014-03-14 10:42:12 +0100

    Initial

---
+++ README.md
@@ -0,0 +1,38 @@
+Contao Extension: TinyMceLocalStorage
+=====================================
+
+Special TinyMCE plugin for temporarily storing texts in the local storage of the browser and restoring them (e.g. after unnoticed logout, when session timed out).
+
+Special thanks to Lingo4you aka @Babelfisch for the javascript code.
+
+
+Installation
+------------
+
+The extension is not published in contao extension repository.
+Install it manually.
+
+
+Tracker
+-------
+
+https://github.com/cliffparnitzky/TinyMceLocalStorage/issues
+
+
+Compatibility
+-------------
+
+- min. version: Contao 3.0.x
+- max. version: Contao 3.2.x
+
+
+Dependency
+----------
+
+- To load this plugin and add it to the configuration the extension [[TinyMcePluginLoader]](https://github.com/cliffparnitzky/TinyMcePluginLoader) has to be installed.
+
+
+Screenshot
+----------
+
+![Screenshot](https://raw.github.com/cliffparnitzky/TinyMceLocalStorage/master/screenshot.jpg)
---
+++ system/modules/TinyMceLocalStorage/TinyMceLocalStorage.php
@@ -0,0 +1,80 @@
+<?php if (!defined('TL_ROOT')) die('You cannot access this file directly!');
+
+/**
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2014 Leo Feyer
+ *
+ * Formerly known as TYPOlight Open Source CMS.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, please visit the Free
+ * Software Foundation website at <http://www.gnu.org/licenses/>.
+ *
+ * PHP version 5
+ * @copyright  Cliff Parnitzky 2014
+ * @author     Cliff Parnitzky
+ * @package    TinyMceLocalStorage
+ * @license    LGPL
+ */
+
+/**
+* Class TinyMceLocalStorage
+*
+* Class to implement the HOOK for adding configs.
+* @copyright  Cliff Parnitzky 2014
+* @author     Cliff Parnitzky
+*/
+class TinyMceLocalStorage {
+	
+	/**
+	 * Adding config for output behavoir
+	 */
+	public function editTinyMcePluginLoaderConfig ($arrTinyConfig) {
+		$arrTinyConfig["setup"] = $this->getJavaScript();
+		return $arrTinyConfig;
+	}
+	
+	/**
+	 * Returns the minified and translated javascript function.
+	 * Orginal code by Lingo4you aka Babelfisch (https://community.contao.org/de/showthread.php?48900-Offline-speichern-(Local-Storage)-in-TinyMCE&p=315513&viewfull=1#post315513)
+	 */
+	private function getJavaScript () {
+		$js = "
+function(ed)
+{
+	ed.onSubmit.add(function(ed, e) {
+		localStorage.setItem(storeKey, ed.getContent({format:'html'}));
+	});
+	ed.onInit.add(function(ed) {
+		storeKey = document.URL.replace(/&rt=[^&]+&?/, '').replace(/^.*\?/, '').replace(/fee=1/, '').replace(/[=&]/g, '-');
+		storedContent = localStorage.getItem(storeKey);
+		if (storedContent !== null && storedContent != '') {
+			/* TODO get current text and remove [nbsp] */
+			if (storedContent != ed.getContent({format:'html'})) {
+				setTimeout(function() {
+					if (confirm('" . $GLOBALS['TL_LANG']['MSC']['TinyMceLocalStorage']['RestoreConfirm'] . "')) {
+						ed.setContent(storedContent);
+					}
+					localStorage.removeItem(storeKey);
+				}, 1000);
+			}
+		}
+	});
+}
+";
+		$js = str_replace(array("\r", "\n", "\t"), "", $js);
+		return $js;
+	}
+}
+ 
+?>
